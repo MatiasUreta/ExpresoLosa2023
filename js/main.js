cccfca9febd8ff11335163a59284e4e9430c7551
matias <matiasureta28@gmail.com>
2023-12-07 01:47:57 -0300

    agrege animaciones al proyecto

--- js/main.js
+++ js/main.js
@@ -28,3 +28,54 @@ function toggleSidebar() {
     sidebar.classList.toggle("expanded");
 }
 
+
+
+/*ANIMACIONES*/
+// Función para detectar si un elemento está en el viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Escucha el evento de scroll
+window.addEventListener('scroll', function (event) {
+    // Obtén todos los elementos con la clase 'scale-in-ver-center'
+    const elements = document.querySelectorAll('.scale-in-ver-center');
+    elements.forEach(function (element) {
+        if (isInViewport(element)) {
+            // Si el elemento está en el viewport, agrega la clase 'animate'
+            element.classList.add('animate');
+        }
+    });
+}, false);
+
+
+
+/*ANIMACIONES DE FOCUS*/
+// Función para detectar si un elemento está en el viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Escucha el evento de scroll
+window.addEventListener('scroll', function (event) {
+    // Obtén todos los elementos con la clase 'focus-in-expand'
+    const elements = document.querySelectorAll('.focus-in-expand');
+    elements.forEach(function (element) {
+        if (isInViewport(element)) {
+            // Si el elemento está en el viewport, agrega la clase 'animate'
+            element.classList.add('animate');
+        }
+    });
+}, false);
